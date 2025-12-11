d469696fa154142091e74b643e59c292f78bc2af
Sipei Li <sipei@mac.mynetworksettings.com>
2025-12-11 12:00:39 -0500

    ADD query window extend experiment for LightGCN

--- ActiveLearner.py
+++ ActiveLearner.py
@@ -13,11 +13,34 @@ from recommenders.evaluation.python_evaluation import (
     recall_at_k,
 )
 
-from utils import separate_by_gender, ratio_sample_by_gender, oversample_pro, compute_rmse, compute_rocauc
+from utils import (
+    separate_by_gender,
+    ratio_sample_by_gender,
+    oversample_pro,
+    compute_rmse,
+    compute_rocauc,
+    compute_item_coverage_by_gender,
+)
 
 class BaseActiveLearner:
 
-    def __init__(self, model, model_hparams, tr_df, te_df, n_users, n_items, iid_to_gender, q=10, seed=42, oversample=False, ratio=None):
+    def __init__(
+        self, 
+        model, 
+        model_hparams, 
+        tr_df, 
+        te_df, 
+        n_users, 
+        n_items, 
+        iid_to_gender, 
+        q=10, 
+        seed=42, 
+        oversample=False, 
+        ratio=None, 
+        equal_rate_extend=False,
+        equal_rate_extend_percentage=0.2,
+        equal_rate_extend_gamma=3,
+    ):
         """Initialize the active learner.
         
         Args:
@@ -47,6 +70,12 @@ class BaseActiveLearner:
 
             ratio: float
                 Ratio of number of ratings collected at each epoch from female and male users.
+
+            equal_rate_extend: bool
+                Whether we are extending the query window of users with lower response rate to equalize the response rate.
+
+            equal_rate_extend_percentage: float
+                Percentage of users with lowest response rate to extend the query window for, if `equal_rate_extend` is True.
         """
 
         tf.random.set_seed(seed)
@@ -66,7 +95,10 @@ class BaseActiveLearner:
         # experiment settings
         self.oversample = oversample
         self.ratio = ratio
-        
+        self.equal_rate_extend = equal_rate_extend
+        self.equal_rate_extend_percentage = equal_rate_extend_percentage
+        self.equal_rate_extend_gamma = equal_rate_extend_gamma
+
         self.col_user = 'user_iid'
         self.col_item = 'item_iid'
         self.col_rating = 'rating'
@@ -74,6 +106,16 @@ class BaseActiveLearner:
 
         # initialize the known set with 5% of each user
         self.kn_df = self.tr_df.groupby('user_iid').sample(frac=0.05, random_state=self.seed)
+
+        # dict of sets of recommended items for each user
+        self.recommended_items_dict = {}
+        for user_iid in range(self.n_users):
+            self.recommended_items_dict[user_iid] = set()
+
+        # dict of sets of queried items for each user
+        self.queried_items_dict = {}
+        for user_iid in range(self.n_users):
+            self.queried_items_dict[user_iid] = set()
 
     
     def generate_queries(self, model):
@@ -136,6 +178,20 @@ class BaseActiveLearner:
         pro_topk_scores = model.recommend_k_items(pro_te_df, top_k, remove_seen=True)
         unpro_topk_scores = model.recommend_k_items(unpro_te_df, top_k, remove_seen=True)
 
+        # update the recommended items dict
+        for _, row in pro_topk_scores.iterrows():
+            user_iid = row[self.col_user]
+            item_iid = row[self.col_item]
+            self.recommended_items_dict[user_iid].add(item_iid)
+
+        for _, row in unpro_topk_scores.iterrows():
+            user_iid = row[self.col_user]
+            item_iid = row[self.col_item]
+            self.recommended_items_dict[user_iid].add(item_iid)
+        
+        # Compute item coverage for protected and unprotected groups
+        pro_agg_item_coverage, unpro_agg_item_coverage, pro_avg_item_coverage, unpro_avg_item_coverage = compute_item_coverage_by_gender(self.recommended_items_dict, self.iid_to_gender, self.n_items)
+
         params = {'k': top_k,
                   'col_user': self.col_user,
                   'col_item': self.col_item,
@@ -177,6 +233,10 @@ class BaseActiveLearner:
                    "rmse_unpro": unpro_rmse,
                    "rocauc_pro": pro_rocauc,
                    "rocauc_unpro": unpro_rocauc,
+                   "agg_item_coverage_pro": pro_agg_item_coverage,
+                   "agg_item_coverage_unpro": unpro_agg_item_coverage,
+                   "avg_item_coverage_pro": pro_avg_item_coverage,
+                   "avg_item_coverage_unpro": unpro_avg_item_coverage,
                    "count_pro": pro_count,
                    "count_unpro": unpro_count,
                    "percentage": (self.kn_df.shape[0]/ self.tr_df.shape[0]),
@@ -200,10 +260,28 @@ class RatingBasedActiveLearner(BaseActiveLearner):
         q=10, 
         seed=42, 
         oversample=False, 
-        ratio=None
+        ratio=None,
+        equal_rate_extend=False,
+        equal_rate_extend_percentage=0.2,
+        equal_rate_extend_gamma=3,
     ):
 
-        super().__init__(model, model_hparams, tr_df, te_df, n_users, n_items, iid_to_gender, q, seed, oversample, ratio)
+        super().__init__(
+            model, 
+            model_hparams, 
+            tr_df, 
+            te_df, 
+            n_users, 
+            n_items, 
+            iid_to_gender, 
+            q, 
+            seed, 
+            oversample, 
+            ratio, 
+            equal_rate_extend, 
+            equal_rate_extend_percentage,
+            equal_rate_extend_gamma,
+        )
         
         avail_strategies = ['MaxRating', 'MinRating', 'MixRating', 'Random']
         if strategy not in avail_strategies:
@@ -228,61 +306,49 @@ class RatingBasedActiveLearner(BaseActiveLearner):
 
     def generate_queries(self, model):
         
-        user_iids_N = np.arange(self.n_users)
-        
-        # generate rating predictions of all users for all items,
-        # and make items seen in training have predictions of `-np.inf`
-        rating_preds_NM = model.score(user_iids_N, remove_seen=True)
-
-        query_df = []
-
-        for user_iid in user_iids_N:
-
+        def generate_user_query_df(user_iid, rating_preds_NM, q, strategy):
             # items queried before
             queried_bef_items = np.argwhere(self.queried_NM[user_iid, :] > 0)[:, 1]
-            
-            if len(queried_bef_items) == self.n_items:
-                continue
 
+            if len(queried_bef_items) == self.n_items:
+                return pd.DataFrame(columns=['user_iid', 'item_iid', 'rating'])
+            
             # candidate set - all items
             user_df = pd.DataFrame(
                 {
-                    'item_iid': np.arange(self.n_items),
-                    'rating_estimate': rating_preds_NM[user_iid, :],
+                    self.col_item: np.arange(self.n_items),
+                    self.col_prediction: rating_preds_NM[user_iid, :],
                 }
             )
-            
-            # remove the items that have been queried before from candidate set
-            user_df = user_df[~user_df['item_iid'].isin(queried_bef_items)]
 
-            if self.strategy == "MaxRating":
-                user_df_sorted = user_df.sort_values(by='rating_estimate', ascending=False)
+            # remove the items that have been queried before from candidate set
+            user_df = user_df[~user_df[self.col_item].isin(queried_bef_items)]
+
+            if strategy == "MaxRating":
+                user_df_sorted = user_df.sort_values(by=self.col_prediction, ascending=False)
             
             elif self.strategy == "MinRating":
                 user_df_sorted = user_df.sort_values(by='rating_estimate', ascending=True)
             
             elif self.strategy == "MixRating":
-                if self.q % 2 != 0:
-                    raise ValueError("To use MixRating strategy, choose a query list size (`q`) that is divisable by 2.")
+                # if self.q % 2 != 0:
+                #     raise ValueError("To use MixRating strategy, choose a query list size (`q`) that is divisable by 2.")
                 k = self.q // 2
                 high_df = user_df.sort_values(by='rating_estimate', ascending=False)
                 low_df = user_df.sort_values(by='rating_estimate', ascending=True)
 
-                user_df_sorted = pd.concat([high_df[:k], low_df[:k]])
+                user_df_sorted = pd.concat([high_df[:(q-k)], low_df[:k]])
             
             elif self.strategy == "Random":
                 # random choice from the candidate pool,
                 # if the size of dataframe is smaller than k, return all
-                if len(user_df) >= self.q:
-                    user_df_sorted = user_df.sample(self.q)
+                if len(user_df) >= q:
+                    user_df_sorted = user_df.sample(q)
                 else:
                     user_df_sorted = user_df 
             
-            else:
-                raise ValueError("Strategy must be one of: 'MaxRating', 'MinRating', 'MixRating', or 'Random'.")
-            
             # keep the top q items
-            query_items = user_df_sorted.iloc[:self.q]['item_iid'].to_list()
+            query_items = user_df_sorted.iloc[:q][self.col_item].to_list()
             
             # update query record matrix
             for item_iid in query_items:
@@ -294,9 +360,75 @@ class RatingBasedActiveLearner(BaseActiveLearner):
             # query items from training set that are in the top q
             user_query_df = user_tr_df[user_tr_df['item_iid'].isin(query_items)]
             
-            query_df.append(user_query_df[['user_iid', 'item_iid', 'rating']])
+            return user_query_df[['user_iid', 'item_iid', 'rating']]
+
+        user_iids_N = np.arange(self.n_users)
+
+        # keep track of user response rate
+        r_N = np.zeros(self.n_users)
         
-        query_df = pd.concat(query_df, axis=0)
+        # generate rating predictions of all users for all items,
+        # and make items seen in training have predictions of `-np.inf`
+        rating_preds_NM = model.score(user_iids_N, remove_seen=True)
+
+        query_df = []
+
+        count = 0
+
+        for user_iid in user_iids_N:
+
+            user_query_df = generate_user_query_df(user_iid, rating_preds_NM, self.q,self.strategy)
+
+            if not user_query_df.empty:
+                query_df.append(user_query_df)
+                r_N[user_iid] = len(user_query_df) / self.q
+
+                count += user_query_df.shape[0]
+        
+        print(f"Total number of collected ratings before extending: {count}")
+        
+        if self.equal_rate_extend:
+            
+            # compute the 5th and 95th percentile of user response rate
+            r_min, r_max = np.percentile(r_N, [5, 95])
+
+            if r_max != r_min:
+
+                print("Extending the query window size for users with low response rate...")
+
+                # clip the user response rate between the lower and upper percentiles for calculation of the new query window size
+                tilde_r_N = np.clip(r_N, r_min, r_max)
+
+                # the proportion to extend the query window size is (r_max - tilde_r_n) / (r_max - r_min)
+                gamma = self.equal_rate_extend_gamma
+                f_N = (gamma - 1) * (r_max - tilde_r_N) / (r_max - r_min)
+                
+                # get the users with bottom self.equal_rate_extend_percentage of response rate
+                bottom_r_user_iids = np.argsort(r_N)[:int(self.n_users * self.equal_rate_extend_percentage)]
+
+                # print percentage of female users in bottom r user iids
+                bottom_r_user_genders = []
+                for user_iid in bottom_r_user_iids:
+                    bottom_r_user_genders.append(self.iid_to_gender[user_iid])
+                bottom_r_user_genders = np.array(bottom_r_user_genders)
+                print(f"Percentage of female users in bottom r user iids: {100 * np.mean(bottom_r_user_genders == 'F'):.2f}%")
+                print(f"Percentage of male users in bottom r user iids: {100 * np.mean(bottom_r_user_genders == 'M'):.2f}%")
+
+                for user_iid in bottom_r_user_iids:
+                    # extend the query window size by the proportion f_N
+                    new_q = int(self.q * f_N[user_iid])
+
+                    # generate the query items
+                    user_query_df = generate_user_query_df(user_iid, rating_preds_NM, new_q, self.strategy)
+
+                    if not user_query_df.empty:
+                        query_df.append(user_query_df)
+
+                        count += user_query_df.shape[0]
+        print(f"Total number of collected ratings after extending: {count}")
+
+        
+        query_df = pd.concat(query_df, axis=0).sample(frac=1, random_state=self.seed)
 
         if self.oversample:
             query_df = oversample_pro(query_df, self.iid_to_gender, random_state=self.seed)
